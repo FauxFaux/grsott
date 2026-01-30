af6feb70bb3d4c4cb14dee987c73f7a580d04db3
Chris West (Faux) <git@goeswhere.com>
2026-01-30 11:02:22 +0000

    wip: some decoding

--- src/bin/print.rs
+++ src/bin/print.rs
@@ -7,33 +7,110 @@ use pcap_file::pcapng::blocks::enhanced_packet::EnhancedPacketBlock;
 use std::collections::HashMap;
 use std::time::Duration;
 use std::{env, fs};
+use time::UtcDateTime;
 
 #[derive(Default)]
 struct Group {
-    inner: HashMap<(Direction, [u8; 3]), Vec<(Duration, Vec<u8>)>>,
+    inner: HashMap<(Direction, [u8; 3]), Vec<(UtcDateTime, Vec<u8>)>>,
 }
 
-type Columns = HashMap<usize, Vec<(Duration, i32)>>;
+type Columns = HashMap<usize, Vec<(UtcDateTime, i32)>>;
+
+type Header = [u8; 8];
+
+struct Packet {
+    ts: UtcDateTime,
+    dir: Direction,
+    header: Header,
+    body: Box<[u8]>,
+}
 
 fn main() -> Result<()> {
-    let mut group = Group::default();
 
+    let mut packets = Vec::with_capacity(4096);
+    let mut file_id = Vec::with_capacity(32);
     for arg in env::args().skip(1) {
         let f = fs::File::open(&arg)?;
         let mut f = pcap_file::pcapng::PcapNgReader::new(f)?;
-        f.interfaces();
         let mut i = 0;
+        file_id.push((arg.clone(), packets.len()));
         while let Some(v) = f.next_block() {
             match v? {
                 InterfaceDescription(_) => (),
                 EnhancedPacket(packet) => {
-                    print_packet_pcap(&packet, &mut group)
+                    frame_packets_pcap(&packet, |p| packets.push(p))
                         .with_context(|| anyhow!("processing packet #{i} in {arg:?}"))?;
                 }
                 v => bail!("unsupported packet {v:?}"),
             }
             i += 1;
         }
+    }
+
+    packets.sort_by_key(|p| p.ts);
+
+    headline_stats(&packets)?;
+
+    if false {
+        analyse_packets(&packets, &file_id)?;
+    }
+
+    Ok(())
+}
+
+fn headline_stats(packets: &[Packet]) -> Result<()> {
+    for pkt in packets {
+        let [_u0, _seq, _u2, major, len1, len0, n0, n1] = pkt.header;
+        let key = [major, n0, n1];
+        match (pkt.dir, key) {
+            (Direction::FromInverter, [0x06, 0x51, 0x20]) => (),
+            _ => continue,
+        }
+
+        let chunks = decrypt(&pkt.body)[60..]
+            .chunks_exact(4)
+            .map(|chunk| i32::from_be_bytes(chunk.try_into().expect("chunks exact")))
+            .collect_vec();
+
+        let (h,m,s) = pkt.ts.time().as_hms();
+        print!("{} {h:02}:{m:02}:{s:02}", pkt.ts.date());
+        for field in pkt_51_20() {
+            if !field.useful {
+                continue;
+            }
+            let div = field.kind.divide().ok_or_else(|| anyhow!("only numerics"))?;
+            let raw = chunks[usize::from(field.off)];
+            let v = (raw as f64) / (div as f64);
+            print!("{v:9.1}");
+        }
+        println!();
+    }
+
+    let mut i = 0;
+    for field in pkt_51_20() {
+        if !field.useful {
+            continue;
+        }
+        i += 1;
+        println!("              {}{} ", " ".repeat(i * 9), field.name);
+    }
+
+    Ok(())
+}
+
+fn analyse_packets(packets: &[Packet], file_id: &[(String, usize)]) -> Result<()> {
+    let mut group = Group::default();
+
+    for (idx, pkt) in packets.iter().enumerate() {
+        group_up(pkt, &mut group).with_context(|| {
+            let (file_name, file_start) = file_id
+                .iter()
+                .rev()
+                .find(|(_, start)| *start <= idx)
+                .expect("file id lookup");
+            let packet_idx = idx - file_start;
+            anyhow!("grouping packet #{packet_idx} in {file_name:?}")
+        })?;
     }
 
     let mut columns = Columns::with_capacity(32);
@@ -69,14 +146,8 @@ fn main() -> Result<()> {
             counts.len()
         );
 
-        for ((when, packet), count) in counts.iter().sorted_by(|(_, a), (_, b)| b.cmp(a)) {
+        for ((when, packet), count) in counts.iter().sorted_by(|(_, a), (_, b)| b.cmp(a)).take(10) {
             println!("{:4} {:4} {}", count, packet.len(), unambiguous(packet));
-            match (dir, key) {
-                (Direction::FromInverter, [0x06, 0x51, 0x20]) => {
-                    print_51_20(packet, *when, &mut columns)?
-                }
-                _ => (),
-            }
         }
     }
 
@@ -85,7 +156,6 @@ fn main() -> Result<()> {
         // println!("{:?}", entries.iter().map(|(_, v)| *v).counts());
         ten_buckets(&entries.iter().map(|(_, v)| *v).collect::<Vec<_>>());
     }
-
     Ok(())
 }
 
@@ -117,7 +187,7 @@ fn ten_buckets(v: &[i32]) {
     }
 }
 
-fn print_51_20(packet: &[u8], when: Duration, columns: &mut Columns) -> Result<()> {
+fn print_51_20(packet: &[u8], when: UtcDateTime, columns: &mut Columns) -> Result<()> {
     ensure!(
         packet.len() == 228,
         "expected 228 byte packet for 51 20, not {}",
@@ -129,8 +199,7 @@ fn print_51_20(packet: &[u8], when: Duration, columns: &mut Columns) -> Result<(
     // println!("Inverter Serial : {}", inverter_serial);
     let packet = &packet[60..];
     // println!("packet: {}", unambiguous(packet));
-    let now = time::UtcDateTime::from_unix_timestamp(when.as_secs() as i64)?;
-    println!("Timestamp       : {}", now);
+    println!("Timestamp       : {}", when);
 
     let chunks = packet
         .chunks_exact(4)
@@ -168,26 +237,26 @@ enum Direction {
     ToInverter,
 }
 
-fn print_packet_pcap(
+fn frame_packets_pcap(
     packet: &EnhancedPacketBlock,
-    group: &mut Group,
+    yield_packet: impl FnMut(Packet) -> (),
 ) -> Result<()> {
-    print_packet(
+    frame_packets(
         match packet.interface_id {
             0 => Direction::FromInverter,
             1 => Direction::ToInverter,
             v => bail!("unknown interface id {v}"),
         },
         &packet.data,
-        group,
+        yield_packet,
         packet.timestamp,
     )
 }
 
-fn print_packet(
+fn frame_packets(
     dir: Direction,
     data: &[u8],
-    group: &mut Group,
+    mut yield_packet: impl FnMut(Packet) -> (),
     ts_clock: Duration,
 ) -> Result<()> {
     if data.len() < 8 {
@@ -197,7 +266,7 @@ fn print_packet(
     let (header, body) = data.split_at(8);
     let header: [u8; 8] = header.try_into().expect("just split");
 
-    let [_u0, _seq, _u2, major, len1, len0, n0, n1] = header;
+    let [_u0, _seq, _u2, _major, len1, len0, _n0, _n1] = header;
     let len = (len0 as usize) | (len1 as usize) << 8;
 
     let (body, rest) = body
@@ -206,6 +275,44 @@ fn print_packet(
 
     crc_suffixed(&data[..len + header.len()]).ok_or_else(|| anyhow!("incorrect CRC"))?;
 
+    yield_packet(Packet {
+        ts: UtcDateTime::from_unix_timestamp(ts_clock.as_secs() as i64)?
+            + time::Duration::nanoseconds(ts_clock.subsec_nanos() as i64),
+        dir,
+        header,
+        body: body.into(),
+    });
+
+    if !rest.is_empty() {
+        frame_packets(dir, rest, yield_packet, ts_clock)
+            .with_context(|| anyhow!("sub-packet after {len}"))?;
+    }
+
+    Ok(())
+}
+
+fn group_up(pkt: &Packet, group: &mut Group) -> Result<()> {
+    let body: &[u8] = &pkt.body;
+    let decrypted = decrypt(body);
+
+    // println!("{:?} {:12} - major:{major:02x} n0:{n0:02x} n1:{n1:02x} len:{len}", ts.as_millis(), format!("{dir:?}"));
+    // println!("{}", unambiguous(&decrypted));
+
+    let [_u0, _seq, _u2, major, len1, len0, n0, n1] = pkt.header;
+
+    group
+        .inner
+        .entry((pkt.dir, [major, n0, n1]))
+        .or_default()
+        .push((pkt.ts, decrypted));
+
+    ensure!(_u0 == 0, "expected u0==0, not {:?}", pkt.header);
+    ensure!(_u2 == 0, "expected u2==0, not {:?}", pkt.header);
+
+    Ok(())
+}
+
+fn decrypt(body: &[u8]) -> Vec<u8> {
     let key = b"Growatt";
     let mut decrypted = key
         .iter()
@@ -215,26 +322,7 @@ fn print_packet(
         .collect::<Vec<_>>();
     decrypted.pop();
     decrypted.pop();
-    let decrypted = decrypted;
-
-    // println!("{:?} {:12} - major:{major:02x} n0:{n0:02x} n1:{n1:02x} len:{len}", ts.as_millis(), format!("{dir:?}"));
-    // println!("{}", unambiguous(&decrypted));
-
-    group
-        .inner
-        .entry((dir, [major, n0, n1]))
-        .or_default()
-        .push((ts_clock, decrypted));
-
-    ensure!(_u0 == 0, "expected u0==0, not {header:?}");
-    ensure!(_u2 == 0, "expected u2==0, not {header:?}");
-
-    if !rest.is_empty() {
-        print_packet(dir, rest, group, ts_clock)
-            .with_context(|| anyhow!("sub-packet after {len}"))?;
-    }
-
-    Ok(())
+    decrypted
 }
 
 pub fn unambiguous(input: &[u8]) -> String {
